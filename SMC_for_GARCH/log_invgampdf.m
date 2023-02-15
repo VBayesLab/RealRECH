5ead77a266cd6c39304aa15a0755fdc9b93cb848
VBLab <38531689+VBayesLab@users.noreply.github.com>
2023-02-16 10:20:36 +1100

    Delete SMC_for_GARCH directory

--- SMC_for_GARCH/log_invgampdf.m
+++
@@ -1,3 +0,0 @@
-function f = log_invgampdf(x,alpha,beta)
-f = alpha.*log(beta)-gammaln(alpha)-(1+alpha).*log(x)-beta./x;
-
--- SMC_for_GARCH/one_step_forecast.m
+++
@@ -1,5 +0,0 @@
-function sigma2_forecast = one_step_forecast(y_cur,sigma2_cur,w,alpha,beta)
-
-sigma2_forecast = w + beta.*y_cur^2 + alpha.*sigma2_cur;
-
-end
